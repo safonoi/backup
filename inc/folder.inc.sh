4dd37293aa177c48b8ec1caee0730b6c91c79acc
= <safonoi@gmail.com>
2015-06-05 11:04:45 +0300

    rm old files and dirs

--- inc/folder.inc.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for FOLDER in ${FOLDERS[@]}; do
-	echo "Creating folder '${FOLDER}' backup.."
-	FOLDER_ESCAPED=$(echo $FOLDER | sed -e 's/\([\.\/]\)/|/g')
-	tar -czf $APP_DIRECTORY/$BACKUP_DIR'/'`echo $FOLDER_DUMP_FILENAME | sed s/#NAME#/$FOLDER_ESCAPED/g` $FOLDER
-done
--- inc/rotate.inc.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo 'Rotation backup directories..'
-dsh -cM -m $REMOTE_ADDR "find $REMOTE_BACKUP_DIR* -type d -mtime +$KEEP_DAYS -exec rm -rf ./* {} \;"
