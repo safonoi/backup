8ef18f73444584b7d987cd245972d119942b9152
= <safonoi@gmail.com>
2015-07-16 10:30:17 +0300

    Ignore mysql errors and prevent exit from utility after mysqldump crushing

--- driver/mysql.sh
+++ driver/mysql.sh
@@ -5,7 +5,7 @@ if test $MYSQL_DBNAMES
 	for DBNAME in ${MYSQL_DBNAMES[@]}; do
 		echo "Creating backup of the mysql database '${DBNAME}'.."
 		DUMP_NAME_TMP=$DBNAME'.sql'
-		mysqldump -u$MYSQL_USER -p$MYSQL_PASS $DBNAME > $DUMP_NAME_TMP
+		mysqldump -u$MYSQL_USER -p$MYSQL_PASS $DBNAME --skip-lock-tables --force > $DUMP_NAME_TMP || true
 		
 		zip $DUMP_NAME_TMP `echo $MYSQL_DUMP_FILENAME | sed s/#NAME#/$DBNAME/g` $MYSQL_DUMP_ZIP
 
